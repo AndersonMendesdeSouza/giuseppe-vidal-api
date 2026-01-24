6d46887d6a3a3eeb4dc60761ca55bc49c1e55115
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-01-24 16:22:27 -0300

    feat:(fishing place) adjustments flux

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
     origin: '*',
     methods: 'GET,POST,PUT,PATCH,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
-
   // app.useGlobalPipes(
   //   new ValidationPipe({
   //     whitelist: true,
@@ -18,6 +19,11 @@ async function bootstrap() {
   //     transform: true,
   //   }),
   // );
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
